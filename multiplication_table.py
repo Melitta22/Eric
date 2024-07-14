c9d122ce7f73abb80aa2af2e3dbc594f8fb0bbff
Melitta22 <51080220+Melitta22@users.noreply.github.com>
2024-07-14 10:55:13 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,23 @@
+#Write a Python program to print the multiplication table of a given number. The user should be able to input the number for
+# which they want the multiplication table, and the program should display the table up to user-defined range
+start=int(input('Enter the start value : '))
+stop=int(input('Enter the stop value : '))
+num=int(input('Enter the number : '))
+for i in range(start,stop+1):
+    print(num,'x',i,'=',i*num)
+
+
+
+
+
+
+
+
+
+
+
+#numb=int(input('Enter the number : '))
+#intrange = 12;
+#for f in range(1,11,1):
+#    value=(f*numb)
+ #   print(numb,'x',f,'=',numb*f)
